5c79dd58c0bbc24faaf853ca0e32c97407e28486
DarkPark <darkpark@pisem.net>
2016-03-09 00:11:29 +0200

    add rules

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+/**
+ * @author Stanislav Kalashnik <darkpark.main@gmail.com>
+ * @license GNU GENERAL PUBLIC LICENSE Version 3
+ */
+
+'use strict';
+
+// public
+module.exports = {
+    // base rules
+    extends: require.resolve('cjs-eslint-config/.eslintrc.js'),
+
+    env: {
+        node: true,
+        browser: true
+    },
+
+    globals: {
+        debug: false
+    },
+
+    // override any settings from the "parent" configuration
+    rules: {}
+};
